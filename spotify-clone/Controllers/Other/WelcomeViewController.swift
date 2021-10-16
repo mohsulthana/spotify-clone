d378816840c8bb32e6ef798de5d5e2ca2634ab29
Mohammad Sulthan <muhammadsultana@outlook.com>
2021-10-16 16:36:55 +0700

    add Authentication process

--- spotify-clone/Controllers/Other/WelcomeViewController.swift
+++ spotify-clone/Controllers/Other/WelcomeViewController.swift
@@ -8,22 +8,57 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    
+    private let signInButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Sign In with Spotify", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Spotify"
+        view.backgroundColor = .systemGreen
+        view.addSubview(signInButton)
+        
+        // action
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        signInButton.frame = CGRect(
+            x: 20,
+            y: view.height - 50 - view.safeAreaInsets.bottom,
+            width: view.width - 40,
+            height: 50
+        )
     }
-    */
-
+    
+    @objc func didTapSignIn() {
+        let vc = AuthViewController()
+//        vc.completionHandler = { [weak self] success in
+//            DispatchQueue.main.async {
+//                self?.handleSignIn(success: success)
+//            }
+//        }
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    private func handleSignIn(success: Bool) {
+        // log user in or yell at them for error
+        guard success else {
+            let alert = UIAlertController(title: "Oops", message: "Something went wrong when sign in", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+            present(alert, animated: true)
+            return
+        }
+        
+        let mainAppTabBarVC = TabBarViewController()
+        mainAppTabBarVC.modalPresentationStyle = .fullScreen
+        present(mainAppTabBarVC, animated: true)
+    }
 }
